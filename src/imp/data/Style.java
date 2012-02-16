ddd2046a898ec8d03006bc4c3d45fc6644eef5e4
keller91711 <keller91711@ee24517c-4790-49d4-a6dd-07d2184826a9>
2012-02-16 20:26:40 +0000

    Added equals and hashcode for Style (auto-generated).

--- src/imp/data/Style.java
+++ src/imp/data/Style.java
@@ -1565,4 +1565,31 @@ static Polylist midiEvent2polylist(MidiEvent event)
     return sum;
     }
 
+    @Override
+    public boolean equals(Object obj)
+      {
+        if( obj == null )
+          {
+            return false;
+          }
+        if( getClass() != obj.getClass() )
+          {
+            return false;
+          }
+        final Style other = (Style) obj;
+        if( (this.name == null) ? (other.name != null) : !this.name.equals(other.name) )
+          {
+            return false;
+          }
+        return true;
+      }
+
+    @Override
+    public int hashCode()
+      {
+        int hash = 5;
+        hash = 29 * hash + (this.name != null ? this.name.hashCode() : 0);
+        return hash;
+      }
+
   }
