60d298dc5fb0b0edf32c15d42f6b4e17d59b21af
keller91711 <keller91711@ee24517c-4790-49d4-a6dd-07d2184826a9>
2012-03-15 08:39:10 +0000

    Checkpoint. Gutting drum rule display stuff.

--- src/imp/gui/DrumRuleDisplay.java
+++ src/imp/gui/DrumRuleDisplay.java
@@ -23,6 +23,7 @@ package imp.gui;
 import imp.com.CommandManager;
 import imp.com.PlayScoreCommand;
 import imp.data.ChordPart;
+import imp.data.Duration;
 import imp.data.MIDIBeast;
 import imp.data.Score;
 import imp.data.Style;
@@ -140,7 +141,6 @@ public class DrumRuleDisplay extends PatternDisplay implements Playable, Display
     {
         //System.out.println("setting rule text to " + text);
         ruleText = text;
-        drumRuleText.setText(text);
     }
     
     /**
@@ -197,44 +197,13 @@ public class DrumRuleDisplay extends PatternDisplay implements Playable, Display
             ErrorLog.log(ErrorLog.WARNING, "Instrument has no corresponding number: " + instrument);
         }
 
-        //drumInstrumentBox.setSelectedItem((Object) instrument);
     }
-    
-    /*
-     * Adds every drum instrument specified by MIDIBeast.drumNames to the instrument box.
 
-    private void initInstrumentBox(){
-        drumInstrumentBox.removeAllItems();
-        for(int i = 0; i < MIDIBeast.drumNames.length; i++){
-            drumInstrumentBox.addItem(MIDIBeast.drumNames[i]);
-        }      
-    }
-    */
 
-   /**
-     * Changes the appearance of this DrumRuleDisplay to "deselected"
-    **/   
-    public void setDeselectedAppearance() {
-       this.rulePanel.setBackground(new java.awt.Color(255,255,255));
-    }
-    
-    /**
-     * Changes the appearance of this DrumRuleDisplay to "selected" and notifies
-     * DrumPatternDisplay notate (if it exists) to deselect any other rules in the pattern.
-    **/
-    private void setSelectedAppearance() {
-       this.rulePanel.setBackground(new java.awt.Color(255,220,112));
-       
-       if(myParentHolder != null) {
-            myParentHolder.setSelectedInstrument(this);
-            myParentHolder.setSelectedAppearance();
-       }
-    } 
-    
+
     public boolean playMe(double swingVal, int loopCount, double tempo, Score s)
     {
         canPlay();
-        setSelectedAppearance();
         try{
             if(checkStatus()) {
                 String r = this.getPlayRule();
@@ -317,8 +286,9 @@ public class DrumRuleDisplay extends PatternDisplay implements Playable, Display
      /**
      * @return the actual text displpayed in the text field
      **/    
+    @Override
     public String toString() {
-        return drumRuleText.getText();
+        return ruleText;
     }
 
 
@@ -336,11 +306,6 @@ public class DrumRuleDisplay extends PatternDisplay implements Playable, Display
         try {  
             //Check for null rules.
             if(displayText.equals("")){
-                String badText = "WARNING: Please enter a rule.";
-                drumRuleText.setToolTipText(badText);
-                ruleLabel.setIcon(badRule);
-                ruleLabel.setToolTipText(badText);
-                errorMsgRule = "drum instrument rule " + displayText + " is empty";
                 return false;
             }               
             
@@ -358,48 +323,23 @@ public class DrumRuleDisplay extends PatternDisplay implements Playable, Display
                 if(!(java.lang.Character.toString(tokenizedRule.get(i).charAt(0)).equals("X")) &&
                    !(java.lang.Character.toString(tokenizedRule.get(i).charAt(0)).equals("R"))){
                         cannotPlay();
-                        String badText = "WARNING: The character " + java.lang.Character.toString(tokenizedRule.get(i).charAt(0)) + " is not valid.";
-                        drumRuleText.setToolTipText(badText);
-                        ruleLabel.setIcon(badRule);
-                        ruleLabel.setToolTipText(badText);
-                        errorMsgRule = "drum instrument rule " + displayText + " contains an invalid character";
                         return false;
                 }
             }
             
             if(Style.makeStyle(l) == null){
                 cannotPlay();
-                String badText = "WARNING: This rule is syntactically incorrect";
-                drumRuleText.setToolTipText(badText);
-                ruleLabel.setIcon(badRule);
-                ruleLabel.setToolTipText(badText);
-                errorMsgRule = "drum instrument rule " + displayText + " is syntactically incorrect";
-               return false;
+                return false;
             } 
            else if(MIDIBeast.numBeatsInRule(displayText) == -1){
                 cannotPlay();
-                String badText = "WARNING: This rule has an unrecognized rhythm duration";
-                drumRuleText.setToolTipText(badText);
-                ruleLabel.setIcon(badRule);
-                ruleLabel.setToolTipText(badText);
-                errorMsgRule = "drum instrument rule " + displayText + "rule " + displayText + " has an unrecognized rhythm duration";
                 return false;
             }    
             else {
-                String goodText = "To preview this rule, press the play button.";
-                drumRuleText.setToolTipText(goodText);
-                ruleLabel.setIcon(goodRule);
-                ruleLabel.setToolTipText(goodText);
-                errorMsgRule = safeMsgRule;
-                return true;
+                 return true;
             }
         }catch(Exception e) {
             cannotPlay();
-            String badText = "WARNING: Unknown error...unable to include rule in style.";
-            drumRuleText.setToolTipText(badText);
-            ruleLabel.setIcon(badRule);
-            ruleLabel.setToolTipText(badText);
-            errorMsgRule = "drum instrument rule " + displayText + " is syntactically incorrect"; 
             return false;
         }
     }
@@ -410,188 +350,26 @@ public class DrumRuleDisplay extends PatternDisplay implements Playable, Display
      * WARNING: Do NOT modify this code. The content of this method is
      * always regenerated by the Form Editor.
      */
-  // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-  private void initComponents()
-  {
-    java.awt.GridBagConstraints gridBagConstraints;
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
 
-    rulePanel = new javax.swing.JPanel();
-    ruleLabel = new javax.swing.JLabel();
-    drumInstrumentBox = new javax.swing.JComboBox();
-    playRuleBtn = new javax.swing.JButton();
-    drumRuleText = new javax.swing.JTextField();
-
-    setMinimumSize(new java.awt.Dimension(495, 30));
-    setOpaque(false);
-    setPreferredSize(new java.awt.Dimension(495, 30));
-    addMouseListener(new java.awt.event.MouseAdapter()
-    {
-      public void mousePressed(java.awt.event.MouseEvent evt)
-      {
-        formMousePressed(evt);
-      }
-    });
-
-    rulePanel.setBackground(new java.awt.Color(255, 255, 255));
-    rulePanel.setMinimumSize(new java.awt.Dimension(495, 25));
-    rulePanel.setPreferredSize(new java.awt.Dimension(495, 25));
-    rulePanel.addMouseListener(new java.awt.event.MouseAdapter()
-    {
-      public void mousePressed(java.awt.event.MouseEvent evt)
-      {
-        rulePanelMousePressed(evt);
-      }
-    });
-    rulePanel.setLayout(new java.awt.GridBagLayout());
-
-    ruleLabel.setText(" ");
-    ruleLabel.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
-    ruleLabel.addMouseListener(new java.awt.event.MouseAdapter()
-    {
-      public void mousePressed(java.awt.event.MouseEvent evt)
-      {
-        ruleLabelMousePressed(evt);
-      }
-    });
-    gridBagConstraints = new java.awt.GridBagConstraints();
-    gridBagConstraints.gridx = 0;
-    gridBagConstraints.gridy = 0;
-    gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-    rulePanel.add(ruleLabel, gridBagConstraints);
-
-    drumInstrumentBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Acoustic Bass Drum", "Bass Drum 1", "Side Stick", "Acoustic Snare", "Hand Clap", "Electric Snare", "Low Floor Tom", "Closed Hi-Hat", "High Floor Tom", "Pedal Hi-Hat", "Low Tom", "Open Hi-Hat", "Low-Mid Tom", "Hi-Mid Tom" }));
-    drumInstrumentBox.setPreferredSize(new java.awt.Dimension(135, 22));
-    drumInstrumentBox.addMouseListener(new java.awt.event.MouseAdapter()
-    {
-      public void mousePressed(java.awt.event.MouseEvent evt)
-      {
-        drumInstrumentBoxMousePressed(evt);
-      }
-    });
-    drumInstrumentBox.addActionListener(new java.awt.event.ActionListener()
-    {
-      public void actionPerformed(java.awt.event.ActionEvent evt)
-      {
-        drumInstrumentBoxActionPerformed(evt);
-      }
-    });
-    drumInstrumentBox.addFocusListener(new java.awt.event.FocusAdapter()
-    {
-      public void focusGained(java.awt.event.FocusEvent evt)
-      {
-        drumInstrumentBoxFocusGained(evt);
-      }
-    });
-    rulePanel.add(drumInstrumentBox, new java.awt.GridBagConstraints());
-
-    playRuleBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imp/gui/graphics/icons/play.png"))); // NOI18N
-    playRuleBtn.setToolTipText("Click to play rule.");
-    playRuleBtn.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-    playRuleBtn.setIconTextGap(0);
-    playRuleBtn.setMaximumSize(new java.awt.Dimension(20, 20));
-    playRuleBtn.setMinimumSize(new java.awt.Dimension(20, 20));
-    playRuleBtn.setOpaque(false);
-    playRuleBtn.setPreferredSize(new java.awt.Dimension(20, 20));
-    playRuleBtn.addMouseListener(new java.awt.event.MouseAdapter()
-    {
-      public void mousePressed(java.awt.event.MouseEvent evt)
-      {
-        playRuleBtnMousePressed(evt);
-      }
-    });
-    playRuleBtn.addActionListener(new java.awt.event.ActionListener()
-    {
-      public void actionPerformed(java.awt.event.ActionEvent evt)
-      {
-        playRuleBtnActionPerformed(evt);
-      }
-    });
-    gridBagConstraints = new java.awt.GridBagConstraints();
-    gridBagConstraints.gridx = 3;
-    gridBagConstraints.gridy = 0;
-    rulePanel.add(playRuleBtn, gridBagConstraints);
-
-    drumRuleText.setMinimumSize(new java.awt.Dimension(300, 20));
-    drumRuleText.setPreferredSize(new java.awt.Dimension(300, 20));
-    drumRuleText.addActionListener(new java.awt.event.ActionListener()
-    {
-      public void actionPerformed(java.awt.event.ActionEvent evt)
-      {
-        drumRuleTextActionPerformed(evt);
-      }
-    });
-    drumRuleText.addFocusListener(new java.awt.event.FocusAdapter()
-    {
-      public void focusGained(java.awt.event.FocusEvent evt)
-      {
-        drumRuleTextFocusGained(evt);
-      }
-      public void focusLost(java.awt.event.FocusEvent evt)
-      {
-        drumRuleTextFocusLost(evt);
-      }
-    });
-    gridBagConstraints = new java.awt.GridBagConstraints();
-    gridBagConstraints.gridx = 2;
-    gridBagConstraints.gridy = 0;
-    gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-    gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-    rulePanel.add(drumRuleText, gridBagConstraints);
-
-    add(rulePanel);
-  }// </editor-fold>//GEN-END:initComponents
-
-    private void drumRuleTextFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_drumRuleTextFocusGained
-        setSelectedAppearance();
-    }//GEN-LAST:event_drumRuleTextFocusGained
-
-    private void drumInstrumentBoxFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_drumInstrumentBoxFocusGained
-        setSelectedAppearance();
-    }//GEN-LAST:event_drumInstrumentBoxFocusGained
-
-    private void drumRuleTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_drumRuleTextActionPerformed
-        setSelectedAppearance();
-        checkStatus();
-        if(myParentHolder != null)
-            myParentHolder.checkStatus();
-        cm.changedSinceLastSave(true);
-    }//GEN-LAST:event_drumRuleTextActionPerformed
-
-    private void drumRuleTextFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_drumRuleTextFocusLost
-        checkStatus();
-        if(myParentHolder != null)
-            myParentHolder.checkStatus();
-        cm.changedSinceLastSave(true);
-    }//GEN-LAST:event_drumRuleTextFocusLost
-
-    private void playRuleBtnMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_playRuleBtnMousePressed
-        setSelectedAppearance();
-    }//GEN-LAST:event_playRuleBtnMousePressed
-
-    private void drumInstrumentBoxMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_drumInstrumentBoxMousePressed
-        setSelectedAppearance();
-    }//GEN-LAST:event_drumInstrumentBoxMousePressed
-
-    private void ruleLabelMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ruleLabelMousePressed
-        setSelectedAppearance();
-    }//GEN-LAST:event_ruleLabelMousePressed
-
-    private void rulePanelMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_rulePanelMousePressed
-        setSelectedAppearance();
-    }//GEN-LAST:event_rulePanelMousePressed
+        setMinimumSize(new java.awt.Dimension(495, 30));
+        setOpaque(false);
+        setPreferredSize(new java.awt.Dimension(495, 30));
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                formMousePressed(evt);
+            }
+        });
+    }// </editor-fold>//GEN-END:initComponents
 
     private void formMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMousePressed
-        setSelectedAppearance();
-    }//GEN-LAST:event_formMousePressed
 
-    private void drumInstrumentBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_drumInstrumentBoxActionPerformed
-        setSelectedAppearance();
-        cm.changedSinceLastSave(true);        
-    }//GEN-LAST:event_drumInstrumentBoxActionPerformed
+    }//GEN-LAST:event_formMousePressed
 
     public double getPatternLength()
     {
-      return ((float)MIDIBeast.numBeatsInRule(getDisplayText()));
+      return Duration.getDuration(getDisplayText())/BEAT; //((float)MIDIBeast.numBeatsInRule(getDisplayText()));
     }
         
     public double getBeats()
@@ -604,18 +382,8 @@ public class DrumRuleDisplay extends PatternDisplay implements Playable, Display
     {
       return 0;
     }
-    
-    private void playRuleBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_playRuleBtnActionPerformed
-       playMe();
-
-    }//GEN-LAST:event_playRuleBtnActionPerformed
-    
-  // Variables declaration - do not modify//GEN-BEGIN:variables
-  private javax.swing.JComboBox drumInstrumentBox;
-  private javax.swing.JTextField drumRuleText;
-  private javax.swing.JButton playRuleBtn;
-  private javax.swing.JLabel ruleLabel;
-  private javax.swing.JPanel rulePanel;
-  // End of variables declaration//GEN-END:variables
+        
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
    
 }
