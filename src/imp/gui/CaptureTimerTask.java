ed5dd3b212ce956fae5f6c48f62df1c0871306ef
bhowellimpro <bhowellimpro@ee24517c-4790-49d4-a6dd-07d2184826a9>
2012-07-18 00:26:28 +0000

    If necessary, CaptureTimerTask now sleeps and allows the previous capture to finish.

--- src/imp/gui/CaptureTimerTask.java
+++ src/imp/gui/CaptureTimerTask.java
@@ -13,17 +13,36 @@ import java.util.TimerTask;
  */
 public class CaptureTimerTask extends TimerTask
 {
+
     PitchExtractor extractor;
 
-    public CaptureTimerTask(PitchExtractor extractor) {
+    public CaptureTimerTask(PitchExtractor extractor)
+    {
         this.extractor = extractor;
     }
 
     public void run()
-        {
-            synchronized (extractor.thisMeasure) {
-                extractor.isCapturing = true;
-                extractor.thisMeasure.notify();
+    {
+//
+            while (extractor.isCapturing)
+            {
+                try
+                {
+                    //System.out.println("Timer delayed by 3 milliseconds");
+                    Thread.sleep(2);
+                } catch (Exception e)
+                {
+                    System.out.println("Sleep error:\n" + e);
+                }
             }
+
+            extractor.isCapturing = true;
+            long time = System.currentTimeMillis();
+            synchronized (extractor.thisMeasure)
+        {
+            extractor.thisMeasure.notify();
         }
+            System.out.println("Timer triggered audio capture at time " + time);
+        //}
+    }
 }
